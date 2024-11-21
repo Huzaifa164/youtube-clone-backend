01df9e24f50060809cd2a13387798b62e36e607f
Huzaifa Ahmed <huzaifa.ahmed@bts.com>
2024-11-21 15:58:57 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
@@ -11,8 +12,8 @@ connectDB()
         console.log("App initialisation failed ", error);
         process.exit(1);
     })
-    app.listen(port = process.env.PORT || 8000, () => {
-        console.log(`App is listening on port ${port}`);
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
     })
 })
 .catch((error) => {
